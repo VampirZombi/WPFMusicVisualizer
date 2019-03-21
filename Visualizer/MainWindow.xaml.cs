5410875b2f516f086608a721c2fe065221751048
Mate Koroknai <mate@example.com>
2019-03-21 15:08:57 +0100

    added some libraries

--- Visualizer/MainWindow.xaml.cs
+++ Visualizer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NAudio;
+using WPFSoundVisualizationLib;
 
 namespace Visualizer
 {
@@ -25,14 +29,39 @@ namespace Visualizer
         public MainWindow()
         {
             InitializeComponent();
+
+            NAudioEngine soundEngine = NAudioEngine.Instance;
+            soundEngine.PropertyChanged += SoundEngine_PropertyChanged;
+
+            //UIHelper.Bind(soundEngine, "CanStop", StopButton, Button.IsEnabledProperty);
+            //UIHelper.Bind(soundEngine, "CanPlay", PlayButton, Button.IsEnabledProperty);
+            //UIHelper.Bind(soundEngine, "CanPause", PauseButton, Button.IsEnabledProperty);
+            //UIHelper.Bind(soundEngine, "SelectionBegin", repeatStartTimeEdit, TimeEditor.ValueProperty, BindingMode.TwoWay);
+            //UIHelper.Bind(soundEngine, "SelectionEnd", repeatStopTimeEdit, TimeEditor.ValueProperty, BindingMode.TwoWay);
+
+            spectrumAnalyzer.RegisterSoundPlayer(soundEngine);
+            //waveformTimeline.RegisterSoundPlayer(soundEngine);
+
+            //LoadExpressionDarkTheme();
+        }
+
+        private void SoundEngine_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
         }
 
         private void BtnOpenSound_Click(object sender, RoutedEventArgs e)
         {
             soundTrack = new MediaPlayer();
-            Uri uri = new Uri(@"C:\Users\Pénzesné Tóth Márta\Downloads\SugarCoatedShit.wav");
-            soundTrack.Open(uri);
-            soundTrack.Play();
+            string uri;
+            Microsoft.Win32.OpenFileDialog openDialog = new Microsoft.Win32.OpenFileDialog();
+            openDialog.Filter = "(*.mp3)|*.mp3";
+            if (openDialog.ShowDialog() == true)
+            {
+                NAudioEngine.Instance.OpenFile(openDialog.FileName);
+                this.Title = openDialog.FileName;
+            }
+
         }
     }
 }
